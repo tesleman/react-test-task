3c7d0782c3d368430ea9dcd6e572360cf27df498
tesleman <54055976+tesleman@users.noreply.github.com>
2020-07-20 12:28:30 +0300

    Updates

---
+++ precache-manifest.b293858f3c425851dc56117abd8f7183.js
@@ -0,0 +1,242 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfeef24adbee9ca50b7748e415bea954",
+    "url": "/react-test-task/index.html"
+  },
+  {
+    "revision": "f6c2a60e17fbaa4786dc",
+    "url": "/react-test-task/static/css/2.dce947be.chunk.css"
+  },
+  {
+    "revision": "afc94a2aa7c3b38ec990",
+    "url": "/react-test-task/static/css/main.bba7f76b.chunk.css"
+  },
+  {
+    "revision": "f6c2a60e17fbaa4786dc",
+    "url": "/react-test-task/static/js/2.03cd53fe.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-test-task/static/js/2.03cd53fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afc94a2aa7c3b38ec990",
+    "url": "/react-test-task/static/js/main.2a109a21.chunk.js"
+  },
+  {
+    "revision": "1adb4df5858ede265f8e",
+    "url": "/react-test-task/static/js/runtime-main.73a6fb8a.js"
+  },
+  {
+    "revision": "aedc56931b4ec03d13ecbd6f7e81a1d0",
+    "url": "/react-test-task/static/media/LeptopGuy.aedc5693.png"
+  },
+  {
+    "revision": "3326e4d74d3924ee1c882c29f5b571c0",
+    "url": "/react-test-task/static/media/OpenSans-Bold.3326e4d7.woff2"
+  },
+  {
+    "revision": "5a100916f94b0babde0c92aaa8fb80d6",
+    "url": "/react-test-task/static/media/OpenSans-Bold.5a100916.ttf"
+  },
+  {
+    "revision": "7ae9b8ba7886341831bf7c85a0da40d0",
+    "url": "/react-test-task/static/media/OpenSans-Bold.7ae9b8ba.eot"
+  },
+  {
+    "revision": "892667349c5cff6fcf7e40439596b97c",
+    "url": "/react-test-task/static/media/OpenSans-Bold.89266734.woff"
+  },
+  {
+    "revision": "d6291f88056601e360ce6cea4bf676f4",
+    "url": "/react-test-task/static/media/OpenSans-Bold.d6291f88.svg"
+  },
+  {
+    "revision": "5aaceea2d60ddb477c6aafc825eece3d",
+    "url": "/react-test-task/static/media/OpenSans-BoldItalic.5aaceea2.woff2"
+  },
+  {
+    "revision": "7be88e73fea7b64568a450d7c01346b0",
+    "url": "/react-test-task/static/media/OpenSans-BoldItalic.7be88e73.woff"
+  },
+  {
+    "revision": "a54aba83b3d5d7702890c07c31c48147",
+    "url": "/react-test-task/static/media/OpenSans-BoldItalic.a54aba83.svg"
+  },
+  {
+    "revision": "c36b5ac7c2dddf6f525c8d161412ef41",
+    "url": "/react-test-task/static/media/OpenSans-BoldItalic.c36b5ac7.ttf"
+  },
+  {
+    "revision": "ea07932c5245dd421e3d05f579098bc2",
+    "url": "/react-test-task/static/media/OpenSans-BoldItalic.ea07932c.eot"
+  },
+  {
+    "revision": "12e2ed7a180e601bff44253d4c7062ad",
+    "url": "/react-test-task/static/media/OpenSans-ExtraBold.12e2ed7a.woff"
+  },
+  {
+    "revision": "19b56cfcb97fbcc245242fb28232939e",
+    "url": "/react-test-task/static/media/OpenSans-ExtraBold.19b56cfc.ttf"
+  },
+  {
+    "revision": "5211065d7cf88c28086d8f99f8705b71",
+    "url": "/react-test-task/static/media/OpenSans-ExtraBold.5211065d.woff2"
+  },
+  {
+    "revision": "561e4b63e9119235465ec88c8c91f2c8",
+    "url": "/react-test-task/static/media/OpenSans-ExtraBold.561e4b63.eot"
+  },
+  {
+    "revision": "8c5c497a47304f276f99ad05e0c5a395",
+    "url": "/react-test-task/static/media/OpenSans-ExtraBold.8c5c497a.svg"
+  },
+  {
+    "revision": "4595d7f8ce0e7b381abb0254f04f0ec2",
+    "url": "/react-test-task/static/media/OpenSans-ExtraBoldItalic.4595d7f8.ttf"
+  },
+  {
+    "revision": "4f44077586ec12a35ce6778e879d43da",
+    "url": "/react-test-task/static/media/OpenSans-ExtraBoldItalic.4f440775.woff"
+  },
+  {
+    "revision": "5f467e780ed0aead6614d85dcca181e1",
+    "url": "/react-test-task/static/media/OpenSans-ExtraBoldItalic.5f467e78.eot"
+  },
+  {
+    "revision": "9704305e6fd8184b40d526fca315d200",
+    "url": "/react-test-task/static/media/OpenSans-ExtraBoldItalic.9704305e.svg"
+  },
+  {
+    "revision": "bc511bacf828ac9e833e6208292066b8",
+    "url": "/react-test-task/static/media/OpenSans-ExtraBoldItalic.bc511bac.woff2"
+  },
+  {
+    "revision": "383eba0e55ed778006d76428812d343c",
+    "url": "/react-test-task/static/media/OpenSans-Italic.383eba0e.woff2"
+  },
+  {
+    "revision": "525074686dfb8aa36b1b92e29de467ac",
+    "url": "/react-test-task/static/media/OpenSans-Italic.52507468.woff"
+  },
+  {
+    "revision": "9b30f13428e1b4a659aeeab4ac1fff35",
+    "url": "/react-test-task/static/media/OpenSans-Italic.9b30f134.ttf"
+  },
+  {
+    "revision": "d6671d41dde41d355619fe05b44f4aac",
+    "url": "/react-test-task/static/media/OpenSans-Italic.d6671d41.svg"
+  },
+  {
+    "revision": "e487b7cb072550896dde6821d24de2be",
+    "url": "/react-test-task/static/media/OpenSans-Italic.e487b7cb.eot"
+  },
+  {
+    "revision": "39d27e13dce3dfe4cdc70a281ccdf113",
+    "url": "/react-test-task/static/media/OpenSans-Light.39d27e13.woff2"
+  },
+  {
+    "revision": "804037562eabaa5dbefa4a435206d017",
+    "url": "/react-test-task/static/media/OpenSans-Light.80403756.eot"
+  },
+  {
+    "revision": "963eb32907744d9a0d6b98127162808f",
+    "url": "/react-test-task/static/media/OpenSans-Light.963eb329.woff"
+  },
+  {
+    "revision": "d79f021974b1f6bc5c21f31689cde94f",
+    "url": "/react-test-task/static/media/OpenSans-Light.d79f0219.svg"
+  },
+  {
+    "revision": "ecb4572a5e478b107dfcb60c16a7eefa",
+    "url": "/react-test-task/static/media/OpenSans-Light.ecb4572a.ttf"
+  },
+  {
+    "revision": "26f1e68dfbd8b8621e5d07f75571a1f9",
+    "url": "/react-test-task/static/media/OpenSans-LightItalic.26f1e68d.ttf"
+  },
+  {
+    "revision": "6725fc490942895a65f559882a17d524",
+    "url": "/react-test-task/static/media/OpenSans-LightItalic.6725fc49.eot"
+  },
+  {
+    "revision": "97534dd409492b05b11ab96b3dd4a557",
+    "url": "/react-test-task/static/media/OpenSans-LightItalic.97534dd4.woff"
+  },
+  {
+    "revision": "b64e9910811cdcc8df89cec2b5025a9f",
+    "url": "/react-test-task/static/media/OpenSans-LightItalic.b64e9910.svg"
+  },
+  {
+    "revision": "e7cc7120e670a8073073dbc3e4ff0184",
+    "url": "/react-test-task/static/media/OpenSans-LightItalic.e7cc7120.woff2"
+  },
+  {
+    "revision": "55835483c304eaa8477fea2c36abba17",
+    "url": "/react-test-task/static/media/OpenSans-Regular.55835483.woff2"
+  },
+  {
+    "revision": "a35546eef3ea0de0d4735f23b0a0873b",
+    "url": "/react-test-task/static/media/OpenSans-Regular.a35546ee.eot"
+  },
+  {
+    "revision": "ac327c4db6284ef64ebe872b6308f5da",
+    "url": "/react-test-task/static/media/OpenSans-Regular.ac327c4d.woff"
+  },
+  {
+    "revision": "cd7296352d159532b66c07d98efb1126",
+    "url": "/react-test-task/static/media/OpenSans-Regular.cd729635.ttf"
+  },
+  {
+    "revision": "f641a7d4e80fd6321135b1a2b4ce8bb1",
+    "url": "/react-test-task/static/media/OpenSans-Regular.f641a7d4.svg"
+  },
+  {
+    "revision": "08952b029e4decbc8ef9fb553cae8cea",
+    "url": "/react-test-task/static/media/OpenSans-Semibold.08952b02.woff2"
+  },
+  {
+    "revision": "0ea04502930623aa3de110aac9f9e4fd",
+    "url": "/react-test-task/static/media/OpenSans-Semibold.0ea04502.eot"
+  },
+  {
+    "revision": "33f225b8f5f7d6b34a0926f58f96c1e9",
+    "url": "/react-test-task/static/media/OpenSans-Semibold.33f225b8.ttf"
+  },
+  {
+    "revision": "9f2144213fad53d4e0fdb26ecf93865f",
+    "url": "/react-test-task/static/media/OpenSans-Semibold.9f214421.woff"
+  },
+  {
+    "revision": "bb100c995f1d20b8a96462fe5f528bd4",
+    "url": "/react-test-task/static/media/OpenSans-Semibold.bb100c99.svg"
+  },
+  {
+    "revision": "1c0b4eb93fcf561eec03297a24922d6c",
+    "url": "/react-test-task/static/media/OpenSans-SemiboldItalic.1c0b4eb9.ttf"
+  },
+  {
+    "revision": "3343e54368719e3786f78a1b22839455",
+    "url": "/react-test-task/static/media/OpenSans-SemiboldItalic.3343e543.woff2"
+  },
+  {
+    "revision": "da061416028fc9a66fbc0b80d33d6e6f",
+    "url": "/react-test-task/static/media/OpenSans-SemiboldItalic.da061416.eot"
+  },
+  {
+    "revision": "ddc348f204283c4f4090a3ed03853054",
+    "url": "/react-test-task/static/media/OpenSans-SemiboldItalic.ddc348f2.svg"
+  },
+  {
+    "revision": "ec55f263e2b86bc0f28fff46b873d6df",
+    "url": "/react-test-task/static/media/OpenSans-SemiboldItalic.ec55f263.woff"
+  },
+  {
+    "revision": "f5443d74025f2a6069e0ca0142c08a05",
+    "url": "/react-test-task/static/media/banner-photo.f5443d74.jpg"
+  },
+  {
+    "revision": "146e29265bcc680129e263a9478dca00",
+    "url": "/react-test-task/static/media/logo_1.146e2926.svg"
+  }
+]);
